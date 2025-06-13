9f07d5301a8808d19247ce8d443acdcf2a07952f
Lucas Silva <lucas.silva@domain.fulltime>
2025-06-13 13:56:28 -0300

    fetch data on tab state change

--- public/main.js
+++ public/main.js
@@ -1,35 +1,13 @@
 const dataContainer = document.querySelector("#data-container")
+let lastFetched = 0
 
-function decimalToTime(decimal) {
-  const hours = Math.floor(decimal)
-  const minutes = Math.round((decimal - hours) * 60)
-
-  return `${hours}h ${minutes < 10 ? "0" + minutes : minutes}min`
-}
-
-function addDecimalTimeToDate(originalDate, decimal) {
-  if (!(originalDate instanceof Date) || isNaN(originalDate.getTime())) {
-    throw new TypeError("Invalid input: originalDate must be a valid Date object.")
-  }
-
-  const hours = Math.floor(decimal)
-  const minutes = Math.round((decimal - hours) * 60)
-
-  const MS_PER_SECOND = 1000
-  const SECONDS_PER_MINUTE = 60
-  const MINUTES_PER_HOUR = 60
-
-  const newDate = new Date(originalDate.getTime())
-
-  const hoursInMs = hours * MINUTES_PER_HOUR * SECONDS_PER_MINUTE * MS_PER_SECOND
-  const minutesInMs = minutes * SECONDS_PER_MINUTE * MS_PER_SECOND
-
-  newDate.setTime(newDate.getTime() + (hoursInMs + minutesInMs))
-
-  const hrs = newDate.getHours()
-  const mins = newDate.getMinutes()
-
-  return `${hrs}:${mins < 10 ? "0" + mins : mins}`
+function fetchData() {
+  lastFetched = new Date().getTime()
+  
+  fetch("/data")
+    .then(res => res.json())
+    .then(async ({ data }) => renderData(data))
+    .catch(console.error)
 }
 
 function renderData(serverData) {
@@ -61,11 +39,14 @@ function renderData(serverData) {
 
 function getData() {
   setInterval(() => {
-    fetch("/data")
-      .then(res => res.json())
-      .then(async ({ data }) => renderData(data))
-      .catch(console.error)
+    if (!document.hidden) fetchData()
   }, 60_000)
 }
+
+document.addEventListener("visibilitychange",() =>{
+  if (!document.hidden && new Date().getTime() - lastFetched > 30_000) {
+    fetchData()
+  }
+})
 
 getData()
--- public/widgets.js
+++ public/widgets.js
@@ -58,3 +58,35 @@ function renderDate(k, date) {
   container.append(renderKeyValue(k, date))
   return container
 }
+
+function decimalToTime(decimal) {
+  const hours = Math.floor(decimal)
+  const minutes = Math.round((decimal - hours) * 60)
+
+  return `${hours}h ${minutes < 10 ? "0" + minutes : minutes}min`
+}
+
+function addDecimalTimeToDate(originalDate, decimal) {
+  if (!(originalDate instanceof Date) || isNaN(originalDate.getTime())) {
+    throw new TypeError("Invalid input: originalDate must be a valid Date object.")
+  }
+
+  const hours = Math.floor(decimal)
+  const minutes = Math.round((decimal - hours) * 60)
+
+  const MS_PER_SECOND = 1000
+  const SECONDS_PER_MINUTE = 60
+  const MINUTES_PER_HOUR = 60
+
+  const newDate = new Date(originalDate.getTime())
+
+  const hoursInMs = hours * MINUTES_PER_HOUR * SECONDS_PER_MINUTE * MS_PER_SECOND
+  const minutesInMs = minutes * SECONDS_PER_MINUTE * MS_PER_SECOND
+
+  newDate.setTime(newDate.getTime() + (hoursInMs + minutesInMs))
+
+  const hrs = newDate.getHours()
+  const mins = newDate.getMinutes()
+
+  return `${hrs}:${mins < 10 ? "0" + mins : mins}`
+}
