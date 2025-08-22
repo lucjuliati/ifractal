ab1895e00ca788de76f14c1646285c09a881d81b
Lucas Silva <lucas.silva@domain.fulltime>
2025-08-22 14:41:16 -0300

    content display bug fix

--- public/scripts/report.js
+++ public/scripts/report.js
@@ -6,17 +6,18 @@ async function getReport(e) {
   if (!e.target.value) return
 
   let date = new Date(`${e.target.value} 12:00:00`)
-  
+
   if (date.toString() == "Invalid Date") return
 
   date.setDate(date.getDate() - 1)
   date = date.toISOString().split("T")[0]
 
   dialog.showModal()
+  modalContent.innerHTML = ""
 
   await fetch(`/report?date=${date}`).then(async (res) => {
     const data = await res.json()
-    
+
     const itens = [
       renderKeyValue("Total", data?.totalWorkedTime),
       renderKeyValue("Data", data.str_data),
